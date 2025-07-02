f765fd3bdef45723d8f5c064df616b60634d5881
MyzTyn <69483519+MyzTyn@users.noreply.github.com>
2025-07-02 17:00:13 +0900

    Simple Main Menu & Basic Components

---
+++ web-project/src/components/ChoicesBox.vue
@@ -0,0 +1,12 @@
+<script setup>
+import ChoiceButton from './ChoiceButton.vue'
+</script>
+
+<template>
+  <div id="choices-container" class="grid grid-cols-2 gap-4">
+    <ChoiceButton></ChoiceButton>
+    <ChoiceButton></ChoiceButton>
+    <ChoiceButton></ChoiceButton>
+    <ChoiceButton></ChoiceButton>
+  </div>
+</template>
