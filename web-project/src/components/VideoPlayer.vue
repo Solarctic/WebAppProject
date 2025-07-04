b2911407e48c76049d66dac776a32a616c05264a
MyzTyn <69483519+MyzTyn@users.noreply.github.com>
2025-07-04 03:09:59 +0000

    Video Player: Visual Improvement

--- web-project/src/components/VideoPlayer.vue
+++ web-project/src/components/VideoPlayer.vue
@@ -1,6 +1,6 @@
 <template>
   <video
     id="video-container"
-    class="flex-1 min-h-0 m-4 bg-black rounded-[15px] flex justify-center items-center"
+    class="flex-1 min-h-0 m-4 max-w-fit bg-black rounded-[15px] self-center"
   ></video>
 </template>
